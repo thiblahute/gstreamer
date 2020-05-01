2800f108ec538b63c2e478a279f45c8ebc1b621c
Thibault Saunier <tsaunier@igalia.com>
2020-05-01 11:33:38 -0400

    ges: Also track children props 'duplicates' in TimelineElement
    We used to only track the first one but this was wrong, so we start
    tracking all the children properties here, adapting the test which
    was already thought for this to be implemented.
    
    At the same time add some flags to determine how children properties
    need to be handled adding a mode that means that all duplicated
    children props will be set together when the user sets that particular
    child property. This is going to be tested in a following commit.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/918>

--- subprojects/gst-editing-services/ges/ges-track-element.c
+++ subprojects/gst-editing-services/ges/ges-track-element.c
@@ -279,9 +279,9 @@ ges_track_element_dispose (GObject * object)
 static void
 ges_track_element_set_asset (GESExtractable * extractable, GESAsset * asset)
 {
+  gchar *tmp;
   GESTrackElementClass *class;
   GstElement *nleobject;
-  gchar *tmp;
   GESTrackElement *object = GES_TRACK_ELEMENT (extractable);
 
   if (ges_track_element_get_track_type (object) == GES_TRACK_TYPE_UNKNOWN) {
@@ -300,7 +300,7 @@ ges_track_element_set_asset (GESExtractable * extractable, GESAsset * asset)
     return;
   }
 
-  tmp = g_strdup_printf ("nle%s", GES_TIMELINE_ELEMENT_NAME (object));
+  tmp = g_strdup_printf ("nleges%s", GES_TIMELINE_ELEMENT_NAME (object));
   gst_object_set_name (GST_OBJECT (nleobject), tmp);
   g_free (tmp);
 
